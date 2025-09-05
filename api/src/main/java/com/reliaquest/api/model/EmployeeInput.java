af5f608e22a8846c606569d18bc6d5239c04885f
Mohit Ladia <ladiamohit92@gmail.com>
2025-09-05 14:15:32 +0530

    initial commit

--- api/src/main/java/com/reliaquest/api/model/EmployeeInput.java
+++ api/src/main/java/com/reliaquest/api/model/EmployeeInput.java
@@ -1,6 +1,5 @@
 package com.reliaquest.api.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.*;
 import lombok.*;
 
@@ -11,21 +10,17 @@ import lombok.*;
 public class EmployeeInput {
 
     @NotBlank(message = "Name must not be blank")
-    @JsonProperty("employee_name")
     private String name;
 
     @NotNull(message = "Salary must be provided")
     @Min(value = 1, message = "Salary must be greater than zero")
-    @JsonProperty("employee_salary")
     private Integer salary;
 
     @NotNull(message = "Age must be provided")
     @Min(value = 16, message = "Age must be at least 16")
     @Max(value = 75, message = "Age must not exceed 75")
-    @JsonProperty("employee_age")
     private Integer age;
 
     @NotBlank(message = "Title must not be blank")
-    @JsonProperty("employee_title")
     private String title;
 }
